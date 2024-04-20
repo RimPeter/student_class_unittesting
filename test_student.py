a08304db3788f3eab95867936725058cfb5fcd1f
RimPeter <hull@localappliancerentals.co.uk>
2024-04-20 08:12:11 +0100

    Building a Method Using TDD - Creating the Test First

--- test_student.py
+++ test_student.py
@@ -6,6 +6,11 @@ class TestStudent(unittest.TestCase):
     def test_full_name(self):
         student = Student("John", "Doe")
         self.assertEqual(student.full_name, "John Doe")
+        
+    def test_alert_santa(self):
+        student = Student("John", "Doe")
+        student.alert_santa()
+        self.assertTrue(student.naughty_list)
 
 if __name__ == '__main__':
     unittest.main()
